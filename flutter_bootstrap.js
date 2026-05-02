3beab7ef15b7466be56e6272347c623e770d0215
yasmenhosam412-yas <yasmenhosam412@gmail.com>
2026-05-02 00:16:37 +0000

    Deploying to gh-pages from @ yasmenhosam412-yas/reset@a214db647032fd07b1d1fe732e0ff75bd5e4524b 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4059741264" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1289676015" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
